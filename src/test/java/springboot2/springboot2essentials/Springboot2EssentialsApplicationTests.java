c8070a5df9a4dafda8a7b3ed49b00fde07e1ef9c
AlefMemTav <190115548@aluno.unb.br>
2022-12-19 15:13:43 -0300

    Initializr

---
+++ src/test/java/springboot2/springboot2essentials/Springboot2EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot2.springboot2essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
