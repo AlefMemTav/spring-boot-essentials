9ab505913cc907d6dc7b98fb9dab38947cbfcdb8
AlefMemTav <190115548@aluno.unb.br>
2022-12-19 14:34:14 -0300

    SpringBootAplication

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -1,17 +1,26 @@
 
 package springboot2.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springboot2.domain.Anime;
+import springboot2.util.DateUtil;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+    private final DateUtil dateUtil;
+
     @GetMapping(path = "list")
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DB2"), new Anime("Berserk"));
     }
 }
