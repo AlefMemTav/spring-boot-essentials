3ea7a9ef842786db5ba325430d73241ebdbe475d
AlefMemTav <190115548@aluno.unb.br>
2022-12-19 14:34:14 -0300

    SpringBootApplication

--- src/main/java/springboot2/util/DateUtil.java
+++ src/main/java/springboot2/util/DateUtil.java
@@ -3,7 +3,9 @@ package springboot2.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DateUtil {
     public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDataTime){
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDataTime);
