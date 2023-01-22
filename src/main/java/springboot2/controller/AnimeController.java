ebb299298ddb24d0a7ba2cef7fc76a63446e874b
AlefMemTav <190115548@aluno.unb.br>
2023-01-22 13:05:30 -0300

    Delete

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -39,4 +39,10 @@ public class AnimeController {
     public ResponseEntity<Anime> save(@RequestBody Anime anime){
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
+
+    @DeleteMapping(path="{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
