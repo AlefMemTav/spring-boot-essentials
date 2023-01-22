20ae6bf9914122bf3b13cde71e98401ed718549b
AlefMemTav <190115548@aluno.unb.br>
2023-01-22 13:11:58 -0300

    Put

--- src/main/java/springboot2/controller/AnimeController.java
+++ src/main/java/springboot2/controller/AnimeController.java
@@ -45,4 +45,10 @@ public class AnimeController {
         animeService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
