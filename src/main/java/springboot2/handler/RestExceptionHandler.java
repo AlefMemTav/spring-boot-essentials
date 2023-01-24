ab22b0dd2d52c634b417959465b3ed676236b7c2
AlefMemTav <190115548@aluno.unb.br>
2023-01-24 17:41:24 -0300

    Handler

---
+++ src/main/java/springboot2/handler/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package springboot2.handler;
+
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import springboot2.exception.BadRequestException;
+import springboot2.exception.BadRequestExceptionDetails;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(badRequestException.getMessage())
+                        .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+}
