a11611fbcc5b3fa0f18dfad214b21405b32a6b98
AlefMemTav <190115548@aluno.unb.br>
2023-02-15 22:32:07 -0300

    RestTemplate part 1

---
+++ src/main/java/springboot2/client/SpringClient.java
@@ -0,0 +1,19 @@
+package springboot2.client;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import springboot2.domain.Anime;
+
+@Log4j2
+public class SpringClient {
+
+    public static void main(String[] args) {
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 11);
+        log.info(entity);
+
+        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 11);
+
+        log.info(object);
+    }
+}
