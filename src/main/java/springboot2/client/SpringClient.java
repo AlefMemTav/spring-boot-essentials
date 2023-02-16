b813e1888fead0bf2782c428ff98b081019ea861
AlefMemTav <190115548@aluno.unb.br>
2023-02-16 09:34:29 -0300

    RestTemplate PUT and DELETE

--- src/main/java/springboot2/client/SpringClient.java
+++ src/main/java/springboot2/client/SpringClient.java
@@ -35,10 +35,6 @@ public class SpringClient {
         //@formatter:on
         log.info(exchange.getBody());
 
-//PostForObject
-//        Anime kingdom = Anime.builder().name("kingdom").build();
-//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
-//        log.info("saved anime {}", kingdomSaved);
         Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
         ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
                 HttpMethod.POST,
@@ -46,6 +42,25 @@ public class SpringClient {
                 Anime.class);
 
         log.info("saved anime {}", samuraiChamplooSaved);
+
+        Anime animeToBeUpdated = samuraiChamplooSaved.getBody();
+
+        animeToBeUpdated.setName("Samurai Champloo 2");
+
+        ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.PUT,
+                new HttpEntity<>(animeToBeUpdated, createJsonHeader()),
+                Void.class);
+
+        log.info(samuraiChamplooUpdated);
+
+        ResponseEntity<Void> samuraiChamplooDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                animeToBeUpdated.getId());
+
+        log.info(samuraiChamplooDeleted);
     }
 
     private static HttpHeaders createJsonHeader() {
