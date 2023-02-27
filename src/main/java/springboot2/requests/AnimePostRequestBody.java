04c2762f66d20e8ee9b88313f1690b672f7b2b0d
AlefMemTav <190115548@aluno.unb.br>
2023-02-27 01:19:17 -0300

    Integration tests part 2

--- src/main/java/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/springboot2/requests/AnimePostRequestBody.java
@@ -4,9 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
 
     @NotEmpty(message = "The anime name cannot be empty")
