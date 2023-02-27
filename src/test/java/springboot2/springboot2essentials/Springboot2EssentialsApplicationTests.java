84ba6dd03d8486fa6c187a22e6298a21046f4ff2
AlefMemTav <190115548@aluno.unb.br>
2023-02-27 01:36:57 -0300

    Maven profile

--- src/test/java/springboot2/springboot2essentials/Springboot2EssentialsApplicationTests.java
+++ src/test/java/springboot2/springboot2essentials/Springboot2EssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package springboot2.springboot2essentials;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Springboot2EssentialsApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
